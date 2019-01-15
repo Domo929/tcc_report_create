3aff8017ed56a29fc063eb979768a33bb5157520
Dominic Cupo <Dominic.Cupo@cepower.net>
2019-01-15 12:25:25 -0500

    Commented the main file. Updated README with usage

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 #TCC_Report_Create
 ##### Dominic Cupo
+
+## Usage
+There are currently two methods to use for this program.
+
+### Method #1 - `default` flag. 
+
+Provide the program with the `--default` flag. The program will look for files in the predetermined file structure. 
+
+```
+Working_Folder/ (call script from here)
+    PDF/
+        Coordination.pdf
+    TCCs/
+        TCC_Base_v*.pdf
+        TCC_Rec_v*.pdf
+```
+
+Note that the `Working_Folder` is not necessarily where the python script is, just where it is called from
+
+### Method #2 - Individual file hooks
+If using this method, do not give the `--default flag`, instead, you will individually point the program at the three files. 
+
+The commands are:
+
+`--cord_path` - The path to the coordination file
+
+`--base_path` - The path to the Base TCC file
+
+`--rec_path` - The path to the Recommended TCC File
+
+If the file has a space in the name, wrap the entire path to the file in `" "`, e.g. :
+
+`--cord_path "C:/path/to/file with space.pdf"`
+
+You can use `/` instead of the normal windows separator `\ ` without issue.
--- tcc_report_create/main.py
+++ tcc_report_create/main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+# ######### IMPORTS ######### #
 from __future__ import print_function
 
 import argparse
@@ -10,10 +10,12 @@ import sys
 from PyPDF3 import PdfFileWriter, PdfFileReader
 
 
+# ######### ERROR HElPER ######### #
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
 
+# ######### ARGUMENT HANDLER ######### #
 parser = argparse.ArgumentParser(description='Create TCC Reports correctly.')
 parser.add_argument('--default',
                     action='store_true',
@@ -26,80 +28,111 @@ parser.add_argument('--base_path',
                     action='store',
                     help='The path to the Base TCC PDF',
                     type=str)
-parser.add_argument('--recc_path',
+parser.add_argument('--rec_path',
                     action='store',
                     help='The path to the Recommended TCC PDF',
                     type=str)
-
+# Store the arguments in a dict for easy reference later
 opts = vars(parser.parse_args())
 
+# ######### PATH HANDLING ######### #
+# Empty paths to be filled depending on the mode chosen (default or otherwise)
 cord_path = ''
 base_path = ''
-recc_path = ''
+rec_path = ''
 
+# If the default flag was not selected
 if not opts['default']:
+    # Fill the paths. If they weren't provided they will be null
     cord_path = opts['cord_path']
     base_path = opts['base_path']
-    recc_path = opts['recc_path']
+    rec_path = opts['rec_path']
 
-    if not (cord_path and base_path and recc_path):
-        eprint('Didn\'t specify default, but also didn\'t give all the individual flags')
+    # Check that if default was NOT selected, we are provided with the proper file flags
+    if not (cord_path and base_path and rec_path):
+        eprint('Didn\'t specify default, but also didn\'t give all the individual flags',
+               'Either provide all three "--EXMP_path" flags OR the "--default" flag')
         exit()
 
+# If default WAS selected
 else:
+    # Get the current working directory. This allows the script to be called from wherever,
+    # and find the files correctly
     cwd = os.getcwd()
 
+    # This allows us to find files that have different versions
     base_glob = 'TCC-Base_v*.pdf'
-    recc_glob = 'TCC-Rec_v*.pdf'
+    rec_glob = 'TCC-Rec_v*.pdf'
 
+    # Build the full path in order to start searching
     cord_path = os.path.join(cwd, 'PDF', 'Coordination.pdf')
     base_path = os.path.join(cwd, 'TCCs', base_glob)
-    recc_path = os.path.join(cwd, 'TCCs', recc_glob)
+    rec_path = os.path.join(cwd, 'TCCs', rec_glob)
 
+    # Build the list of all possible matches to the glob
     base_glob_list = glob.glob(base_path)
+
+    # If there is more than one, ask them to remove the extra
+    # TODO Load a file browser and ask the user to choose which one
     if len(base_glob_list) > 1:
         eprint('Found multiple versions of TCC_Base. Please remove all extras')
         exit()
+    # If there aren't any files in the folder, throw error
+    # TODO Load a file browser and ask the user to choose where the file is
     elif len(base_glob_list) == 0:
         eprint('Found no version of TCC_base. Please provide a sheet')
         exit()
     else:
         base_path = base_glob_list[0]
 
-    recc_glob_list = glob.glob(recc_path)
-    if len(recc_glob_list) > 1:
+    # The same checks for the base file happen for the reccomended file
+    rec_glob_list = glob.glob(rec_path)
+    if len(rec_glob_list) > 1:
         eprint('Found multiple versions of TCC_Rec. Please remove all extras')
         exit()
-    elif len(recc_glob_list) == 0:
+    elif len(rec_glob_list) == 0:
         eprint('Found no version of TCC_rec. Please provide a sheet')
         exit()
     else:
-        recc_path = recc_glob_list[0]
+        rec_path = rec_glob_list[0]
 
-
+# ######### PDF Write Setup ######### #
+# Open the output file writer
 output = PdfFileWriter()
 
+# Open the input PDFs
 cord_in = PdfFileReader(open(cord_path, 'rb'), False)
 base_in = PdfFileReader(open(base_path, 'rb'), False)
-recc_in = PdfFileReader(open(recc_path, 'rb'), False)
+rec_in = PdfFileReader(open(rec_path, 'rb'), False)
 
-if base_in.getNumPages() != recc_in.getNumPages():
+# Check that the same number of pages exist in the Base and Recommended PDFs.
+# They should be the same length
+if base_in.getNumPages() != rec_in.getNumPages():
     eprint('Number of pages does not match in the Base and Recommended PDFs')
     exit()
 
+# Check that the coordination PDF is longer than the base (and therefore recc) pdf too.
+# The Coordination PDF includes pages at the front that do not get sliced in, and instead actually sit
+# in the front. If the Coordination pdf is less than the Base or Recc, these are missing, or there was another error
 if cord_in.getNumPages() < base_in.getNumPages():
     eprint('Coordination PDF is shorter than the Base/Recc PDFs')
     exit()
 
+# ######### ZIPPING ######### #
+# Find the difference in length of the PDFs, these are the leader pages of the coordination
 diff_length = cord_in.getNumPages() - base_in.getNumPages()
 
+# Add the first pages from the coordination PDF to the output PDF
 for ii in range(diff_length):
     output.addPage(cord_in.getPage(ii))
 
+# Now we go through and 'zip' the files together. Note that the Coordination PDF has the diff_length
+# offset added to it to account for the starting pages. Everything else is just the actual indices
 for jj in range(base_in.getNumPages()):
     output.addPage(cord_in.getPage(jj + diff_length))
     output.addPage(base_in.getPage(jj))
-    output.addPage(recc_in.getPage(jj))
+    output.addPage(rec_in.getPage(jj))
 
+# Finally, output everything to the PDF
 with open("8.0 - Coordination Results & Recommendations_CE2018.pdf", "wb") as w:
     output.write(w)
